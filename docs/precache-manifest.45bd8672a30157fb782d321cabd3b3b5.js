3d98239e8a9d4f46700fdb9b3aaa20ee47b26ae5
Tim Wan <ord365@manstrong.com.tw>
2018-12-12 08:44:26 +0800

    加入 docs

---
+++ docs/precache-manifest.45bd8672a30157fb782d321cabd3b3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-play/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59675f2708bb5cf741c2",
+    "url": "/pwa-play/static/js/runtime~main.59675f27.js"
+  },
+  {
+    "revision": "e9e3576e44e435d36fa9",
+    "url": "/pwa-play/static/js/main.e9e3576e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/pwa-play/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "e9e3576e44e435d36fa9",
+    "url": "/pwa-play/static/css/main.b8714570.chunk.css"
+  },
+  {
+    "revision": "31cfce5e887869a96e3b8be595785686",
+    "url": "/pwa-play/index.html"
+  }
+];
