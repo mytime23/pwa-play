744ff1d5bffdf257d5710ee35bde9c70afc88dfa
Tim Wan <ord365@manstrong.com.tw>
2018-12-13 17:20:27 +0800

    發佈並更新到docs

---
+++ docs/precache-manifest.e0bbd07eef5baffc63d73e712de3a9b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-play/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59675f2708bb5cf741c2",
+    "url": "/pwa-play/static/js/runtime~main.59675f27.js"
+  },
+  {
+    "revision": "e4b591a4372894dbab4a",
+    "url": "/pwa-play/static/js/main.e4b591a4.chunk.js"
+  },
+  {
+    "revision": "7c4aa62154bd7ac2cd55",
+    "url": "/pwa-play/static/js/1.7c4aa621.chunk.js"
+  },
+  {
+    "revision": "e4b591a4372894dbab4a",
+    "url": "/pwa-play/static/css/main.a08440f7.chunk.css"
+  },
+  {
+    "revision": "7c4aa62154bd7ac2cd55",
+    "url": "/pwa-play/static/css/1.f8415ec1.chunk.css"
+  },
+  {
+    "revision": "0b39db3dbe7f16953319665d1839c68a",
+    "url": "/pwa-play/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa-play/precache-manifest.b0b0e080f38f74578fb88e9320d556e6.js"
+  "/pwa-play/precache-manifest.e0bbd07eef5baffc63d73e712de3a9b8.js"
 );
 
 workbox.clientsClaim();
