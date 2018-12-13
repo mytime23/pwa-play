4650a2593b38c572e980dcf01d536e578a7cd232
Tim Wan <ord365@manstrong.com.tw>
2018-12-13 17:17:47 +0800

    加入登入畫面測試

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React, { Component } from 'react';
+import {
+  Container, Card, CardHeader, CardBody, CardFooter, Row, Col, Form,
+  FormGroup, Label, Input, Alert,
+  Button,
+} from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      message: ''
+    }
+  }
+
+  handleChangeEmail = (e) => {
+    this.setState({
+      email : e.target.value
+    })
+  }
+ 
+  handleChangePassword = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+
+    if(this.state.email === '') {
+      return this.setState({ message: "Please input Email !"});      
+    }
+
+    if(this.state.password === '') {
+      return this.setState({ message : "Please input Password !"});
+    }
+
+    return this.setState({ message : ""});
+
+  }
+
   render() {
+    let email = this.state.email;
+    let password = this.state.password;
+    const message = (this.state.message ==='')
+          ?""
+          :<Alert color="danger">{this.state.message}</Alert>;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container fluid>
+      <Row className="mt-5">
+          <Col md={{ size: 4, offset: 4 }}>
+              <Card>
+                  <CardHeader><h3><img src={logo} className="App-logo" alt="logo" />Sign In</h3></CardHeader>
+                  <CardBody>
+                      <Form className="form">            
+                          <FormGroup>
+                              <Label>Email</Label>
+                              <Input
+                                  type="email"
+                                  name="email"
+                                  id="exampleEmail"
+                                  placeholder="myemail@email.com"
+                                  value= {email}
+                                  onChange={this.handleChangeEmail}
+                              />
+                          </FormGroup>
+                          <FormGroup>
+                              <Label for="examplePassword">Password</Label>
+                              <Input
+                                  type="password"
+                                  name="password"
+                                  id="examplePassword"
+                                  placeholder="********"
+                                  value={password}
+                                  onChange={this.handleChangePassword} 
+                              />
+                          </FormGroup>                          
+                          <Button className="" onClick={this.handleClick}>Submit</Button>
+                      </Form>
+                  </CardBody>
+                  <CardFooter>
+                     {message}
+                  </CardFooter>
+              </Card>
+          </Col>
+      </Row>
+      </Container>
     );
   }
 }
