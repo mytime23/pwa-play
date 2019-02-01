34fc7dab330adfa5653a8c94916db4195e3198c1
Tim <ord365@manstrong.com.tw>
2019-02-01 15:37:44 +0800

    Updates

---
+++ precache-manifest.82af61cf01e4c58293c25f2975e5a26e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59675f2708bb5cf741c2",
+    "url": "/pwa-play/static/js/runtime~main.59675f27.js"
+  },
+  {
+    "revision": "1e79bbfa1896e8f706e8",
+    "url": "/pwa-play/static/js/main.1e79bbfa.chunk.js"
+  },
+  {
+    "revision": "e48a6d50396bd6e90b5a",
+    "url": "/pwa-play/static/js/1.e48a6d50.chunk.js"
+  },
+  {
+    "revision": "1e79bbfa1896e8f706e8",
+    "url": "/pwa-play/static/css/main.6ac1e8bd.chunk.css"
+  },
+  {
+    "revision": "e48a6d50396bd6e90b5a",
+    "url": "/pwa-play/static/css/1.a085e90d.chunk.css"
+  },
+  {
+    "revision": "d18124faedef7f7439a45e7e01d7c0c6",
+    "url": "/pwa-play/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a9d97f2c23ceadc882a84fafe9965f9.js"
+  "/pwa-play/precache-manifest.82af61cf01e4c58293c25f2975e5a26e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pwa-play/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
